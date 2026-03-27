13b3fbdefdd086b08e52848740dd4c9fee8ed9d9
Genc Balisha <114856687+GenciEB@users.noreply.github.com>
2026-03-27 09:41:26 +0100

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,8 +5,8 @@ const db = require('../db');
 
 const COOKIE_OPTS = {
   httpOnly: true,
-  secure: process.env.NODE_ENV === 'production',
-  sameSite: 'strict',
+  secure:'true',
+  sameSite: 'none',
   maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
 };
 
